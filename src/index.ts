ef85345e9997524ec4ac52a4d9d9dbad8601c729
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2019-05-09 13:57:31 -0300

    ts generated from rcn-diaspore-contract-artifacts#v1.0.2-beta

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
 export { Response } from './response';
 export { BaseContract } from "@0x/base-contract";
-//export * from './generated-wrappers/capped_s_t_o';
+
+export * from './generated-wrappers/cosigner';
+export * from './generated-wrappers/debt_engine';
+export * from './generated-wrappers/erc165';
+export * from './generated-wrappers/erc721_base';
+export * from './generated-wrappers/ierc20';
+export * from './generated-wrappers/ierc165';
+export * from './generated-wrappers/ierc173';
+export * from './generated-wrappers/installments_model';
+export * from './generated-wrappers/loan_approver';
+export * from './generated-wrappers/loan_manager';
+export * from './generated-wrappers/model_descriptor';
+export * from './generated-wrappers/model';
+export * from './generated-wrappers/nano_loan_model';
+export * from './generated-wrappers/oracle_adapter';
+export * from './generated-wrappers/oracle';
+export * from './generated-wrappers/ownable';
+export * from './generated-wrappers/rate_oracle';
+export * from './generated-wrappers/simple_delegable';
+export * from './generated-wrappers/u_r_i_provider';
+
+
+
